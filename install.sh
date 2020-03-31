3a0f9b2680bee0a92546a949af6d04ecf642ecb6
phpliv <61728697+phpliv@users.noreply.github.com>
2020-03-31 17:43:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,4 +18,4 @@ echo -e "\t"\"password\":\""$rndpassword"\""," >> $filename
 echo -e "\t"\"timeout\":600"," >> $filename
 echo -e "\t"\"method\":\"chacha20-ietf\" >> $filename
 echo "}" >> $filename
-sudo ssserver -c $configdir"/"$filename -d start && rm -rf $0 && echo -e "结果\n\033[31mSsserver已经启动\033[0m, 端口是:\033[31m"$rndport"\033[0m, 密码是:\033[31m"$rndpassword"\033[0m, 配置文件位于:\033[31m"$configdir"/"$filename"\033[0m\n\n额外说明:\n启动命令:sudo ssserver -c $configdir"/"$filename -d start\n停止命令:sudo ssserver -c $configdir"/"$filename -d stop\n重启命令:sudo ssserver -c $configdir"/"$filename -d restart"
+sudo ssserver -c $configdir"/"$filename -d start && rm -rf $0 && echo -e "\n结果\n\033[31mSsserver已经启动\033[0m, 端口是:\033[31m"$rndport"\033[0m, 密码是:\033[31m"$rndpassword"\033[0m, 配置文件位于:\033[31m"$configdir"/"$filename"\033[0m\n\n额外说明:\n启动命令: sudo ssserver -c $configdir"/"$filename -d start\n停止命令: sudo ssserver -c $configdir"/"$filename -d stop\n重启命令: sudo ssserver -c $configdir"/"$filename -d restart\n"
