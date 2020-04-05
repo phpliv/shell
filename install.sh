9da4387f6f8f5cc89e78f3eaf48e70890497cf4f
phpliv <61728697+phpliv@users.noreply.github.com>
2020-04-05 08:49:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ function mimvp_app_rand(){
 rndport=$(mimvp_app_rand 1024 65535)
 rndpassword=$(date +%s%N | md5sum | head -c 30)
 filename=config.json
-rndmethod=chacha20-ietf
+rndmethod=xchacha20-ietf-poly1305
 basedir=`cd \`dirname $0\`; pwd`
 configdir=`cd \`dirname $basedir\`; pwd`
 echo "{" > $filename
