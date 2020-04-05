34cc08517fb53ed708613b89e6fb57edcc175ecd
phpliv <61728697+phpliv@users.noreply.github.com>
2020-04-05 10:45:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ function mimvp_app_rand(){
 }
 rndport=$(mimvp_app_rand 1024 65535)
 rndpassword=$(date +%s%N | md5sum | head -c 30)
+ipaddress=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
 filename=config.json
 rndmethod=chacha20-ietf
 basedir=`cd \`dirname $0\`; pwd`
@@ -19,4 +20,4 @@ echo -e "\t"\"password\":\""$rndpassword"\""," >> $filename
 echo -e "\t"\"timeout\":600"," >> $filename
 echo -e "\t"\"method\":\""$rndmethod"\" >> $filename
 echo "}" >> $filename
-sudo ssserver -c $configdir"/"$filename -d start && rm -rf $0 && echo -e "\n\n\033[34mSsserver已经启动\033[0m, 配置文件位于:\033[34m"$configdir"/"$filename"\033[0m, 可自行修改, 但需重启.\n端口:\033[34m"$rndport"\033[0m\n密码:\033[34m"$rndpassword"\033[0m\n加密方法:\033[34m"$rndmethod"\033[0m \n\n说明:\nssserver启动命令: sudo ssserver -c $configdir"/"$filename -d start\nssserver停止命令: sudo ssserver -c $configdir"/"$filename -d stop\nssserver重启命令: sudo ssserver -c $configdir"/"$filename -d restart\n"
+sudo ssserver -c $configdir"/"$filename -d start && rm -rf $0 && echo -e "\n\n\033[34mSsserver已经启动\033[0m, 配置文件位于:\033[34m"$configdir"/"$filename"\033[0m, 可自行修改, 但需重启.\nIP:\033[34m"$ipaddress"\033[0m\n端口:\033[34m"$rndport"\033[0m\n密码:\033[34m"$rndpassword"\033[0m\n加密方法:\033[34m"$rndmethod"\033[0m \n\n说明:\nssserver启动命令: sudo ssserver -c $configdir"/"$filename -d start\nssserver停止命令: sudo ssserver -c $configdir"/"$filename -d stop\nssserver重启命令: sudo ssserver -c $configdir"/"$filename -d restart\n"
